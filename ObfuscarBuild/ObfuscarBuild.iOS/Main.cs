0794ac1be4bde3e26f817ef6b4fad085f08990bf
Chokchai Homsombat <chokchai@perfenterprise.com>
2021-05-13 09:55:41 +0700

    reslove error

--- ObfuscarBuild/ObfuscarBuild.iOS/Main.cs
+++ ObfuscarBuild/ObfuscarBuild.iOS/Main.cs
@@ -16,12 +16,13 @@ namespace ObfuscarBuild.iOS
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
         }
-        public void somthing()
+        public string somthing()
         {
             const string ReadHere = "Can read this string ?";
             string x = ReadHere;
             int aa = 1;
             aa += 1;
+            return x;
         }
     }
 }
